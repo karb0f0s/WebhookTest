8158449b933ef50323a6df86b4ecc9349aa5d562
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2020-11-16 17:51:45 +0300

    Add project files.

---
+++ Webhook/Controllers/UpdateController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace Webhook.Controllers
+{
+    [Route("api/[controller]")]
+    public class UpdateController : Controller
+    {
+        private readonly ChannelWriter<Update> _updatesQueue;
+
+        public UpdateController(Channel<Update> updatesQueue)
+        {
+            _updatesQueue = updatesQueue.Writer;
+        }
+
+        // POST api/update
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {update?.Id} Enqueue...");
+            await _updatesQueue.WriteAsync(update);
+            return Ok();
+        }
+    }
+}
---
+++ Webhook/Services/UpdatesService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot.Types;
+
+namespace Webhook.Services
+{
+    public class UpdatesService : BackgroundService
+    {
+        private readonly ChannelReader<Update> _updatesQueue;
+
+        public UpdatesService(Channel<Update> updatesQueue)
+        {
+            _updatesQueue = updatesQueue.Reader;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await foreach (var update in _updatesQueue.ReadAllAsync(stoppingToken))
+            {
+                //Task.Run(async () =>  {
+                    Console.WriteLine($"[{DateTime.Now}] {update?.Id} Start processing...");
+                    await Task.Delay(10_000);
+                    Console.WriteLine($"[{DateTime.Now}] {update?.Id} Stop processing...");
+                //});
+            }
+        }
+    }
+}
